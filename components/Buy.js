27de9736ba0859f86bcc744bf3a744d7133273aa
Andrey Minin <ewkjf@mail.ru>
2022-08-27 20:13:40 +0400

    turn transactions to payments

--- components/Buy.js
+++ components/Buy.js
@@ -1,15 +1,22 @@
-import React, { useState, useMemo } from "react";
+import React, {useState, useMemo, useEffect} from "react";
 import { Keypair, Transaction } from "@solana/web3.js";
+import { findReference, FindReferenceError } from "@solana/pay";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { InfinitySpin } from "react-loader-spinner";
 import IPFSDownload from "./IpfsDownload";
+
+const STATUS = {
+    Initial: "Initial",
+    Submitted: "Submitted",
+    Paid: "Paid",
+};
 
 export default function Buy({ itemID }) {
     const { connection } = useConnection();
     const { publicKey, sendTransaction } = useWallet();
     const orderID = useMemo(() => Keypair.generate().publicKey, []); // Public key used to identify the order
 
-    const [paid, setPaid] = useState(null);
+    const [status, setStatus] = useState(STATUS.Initial);
     const [loading, setLoading] = useState(false); // Loading state of all above
 
     // useMemo is a React hook that only computes the value if the dependencies change
@@ -43,14 +50,42 @@ export default function Buy({ itemID }) {
             // Send the transaction to the network
             const txHash = await sendTransaction(tx, connection);
             console.log(`Transaction sent: https://solscan.io/tx/${txHash}?cluster=devnet`);
-            // Even though this could fail, we're just going to set it to true for now
-            setPaid(true);
+            setStatus(STATUS.Submitted);
         } catch (error) {
             console.error(error);
         } finally {
             setLoading(false);
         }
     };
+
+    useEffect(() => {
+        // Check if transaction was confirmed
+        if (status === STATUS.Submitted) {
+            setLoading(true);
+            const interval = setInterval(async () => {
+                try {
+                    const result = await findReference(connection, orderID);
+                    console.log("Finding tx reference", result.confirmationStatus);
+                    if (result.confirmationStatus === "confirmed" || result.confirmationStatus === "finalized") {
+                        clearInterval(interval);
+                        setStatus(STATUS.Paid);
+                        setLoading(false);
+                        alert("Thank you for your purchase!");
+                    }
+                } catch (e) {
+                    if (e instanceof FindReferenceError) {
+                        return null;
+                    }
+                    console.error("Unknown error", e);
+                } finally {
+                    setLoading(false);
+                }
+            }, 1000);
+            return () => {
+                clearInterval(interval);
+            };
+        }
+    }, [status]);
 
     if (!publicKey) {
         return (
@@ -66,7 +101,7 @@ export default function Buy({ itemID }) {
 
     return (
         <div>
-            {paid ? (
+            { status === STATUS.Paid ? (
                 <IPFSDownload filename="giphy.gif" hash="Qmcwxi73kCRT9Gf2PLpmKGfb4NDgsTpKCiXraBi3NLKFWu" cta="Download emojis"/>
             ) : (
                 <button disabled={loading} className="buy-button" onClick={processTransaction}>
