2d5193f26b898dd91244fe5a31f7c460bd5ea18f
Andrey Minin <ewkjf@mail.ru>
2022-08-27 18:55:33 +0400

    built online store

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,55 @@
 import React, { useMemo } from "react";
+// wallet-adapter-network is just an enumerable object for the available networks.
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// WalletModalProvider is a fancy React component that will prompt the user to select their wallet.
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+// ConnectionProvider takes an RPC endpoint in and lets us talk directly to the nodes on the Solana blockchain
+// WalletProvider gives us a standard interface for connecting to all sorts of wallets, so we don't have to bother reading docs for each wallet.
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import {
+    GlowWalletAdapter,
+    PhantomWalletAdapter,
+    SlopeWalletAdapter,
+    SolflareWalletAdapter,
+    TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+// clusterApiURL is just a function that generates an RPC endpoint for us based on the network we give it.
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "../styles/globals.css";
 import "../styles/App.css";
 
 const App = ({ Component, pageProps }) => {
+    // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+    const network = WalletAdapterNetwork.Devnet;
 
-  return (
-    <Component {...pageProps} />
-  );
+    // You can also provide a custom RPC endpoint
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            new GlowWalletAdapter(),
+            new SlopeWalletAdapter(),
+            new SolflareWalletAdapter({ network }),
+            new TorusWalletAdapter(),
+        ],
+        [network]
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <Component {...pageProps} />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 };
 
 export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import Product from "../components/Product";
+import { PublicKey } from '@solana/web3.js';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import HeadComponent from '../components/Head';
 
 // Constants
@@ -6,19 +10,46 @@ const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-  
-  
-  return (
+    // This will fetch the users' public key (wallet address) from any wallet we support
+    const { publicKey } = useWallet();
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        if (publicKey) {
+            fetch(`/api/fetchProducts`)
+                .then(response => response.json())
+                .then(data => {
+                    setProducts(data);
+                    console.log("Products", data);
+                });
+        }
+    }, [publicKey]);
+
+    const renderNotConnectedContainer = () => (
+        <div className="button-container">
+            <WalletMultiButton className="cta-button connect-wallet-button" />
+        </div>
+    );
+
+    const renderItemBuyContainer = () => (
+        <div className="products-container">
+            {products.map((product) => (
+                <Product key={product.id} product={product} />
+            ))}
+        </div>
+    );
+
+    return (
     <div className="App">
       <HeadComponent/>
       <div className="container">
         <header className="header-container">
           <p className="header"> 😳 Buildspace Emoji Store 😈</p>
-          <p className="sub-text">The only emoji store that accepts sh*tcoins</p>
+          <p className="sub-text">The most simple emoji store you can imagine</p>
         </header>
 
         <main>
-          <img src="https://media.giphy.com/media/eSwGh3YK54JKU/giphy.gif" alt="emoji" />
+          {publicKey ? renderItemBuyContainer() : renderNotConnectedContainer()}
         </main>
 
         <div className="footer-container">
