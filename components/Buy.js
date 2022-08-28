802cc09a706bcd7925c737515a3bff90c270c532
Andrey Minin <ewkjf@mail.ru>
2022-08-28 12:31:04 +0400

    reading products from our database

--- components/Buy.js
+++ components/Buy.js
@@ -4,7 +4,7 @@ import { findReference, FindReferenceError } from '@solana/pay';
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { InfinitySpin } from 'react-loader-spinner';
 import IPFSDownload from './IpfsDownload';
-import { addOrder } from '../lib/api';
+import { addOrder, hasPurchased, fetchItem } from "../lib/api";
 
 const STATUS = {
     Initial: "Initial",
@@ -17,6 +17,7 @@ export default function Buy({ itemID }) {
     const { publicKey, sendTransaction } = useWallet();
     const orderID = useMemo(() => Keypair.generate().publicKey, []); // Public key used to identify the order
 
+    const [item, setItem] = useState(null); // IPFS hash & filename of the purchased item
     const [loading, setLoading] = useState(false); // Loading state of all above
     const [status, setStatus] = useState(STATUS.Initial);
 
@@ -60,6 +61,21 @@ export default function Buy({ itemID }) {
     };
 
     useEffect(() => {
+        // Check if this address has already purchased this item
+        // If so, fetch the item and set paid to true
+        // Async function to avoid blocking the UI
+        async function checkPurchased() {
+            const purchased = await hasPurchased(publicKey, itemID);
+            if (purchased) {
+                setStatus(STATUS.Paid);
+                const item = await fetchItem(itemID);
+                setItem(item);
+            }
+        }
+        checkPurchased();
+    }, [publicKey, itemID]);
+
+    useEffect(() => {
         // Check if transaction was confirmed
         if (status === STATUS.Submitted) {
             setLoading(true);
@@ -86,6 +102,15 @@ export default function Buy({ itemID }) {
             return () => {
                 clearInterval(interval);
             };
+        }
+
+        async function getItem(itemID) {
+            const item = await fetchItem(itemID);
+            setItem(item);
+        }
+
+        if (status === STATUS.Paid) {
+            getItem(itemID);
         }
     }, [status]);
 
